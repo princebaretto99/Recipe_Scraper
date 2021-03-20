5574f8585dabf3f4c48da476a4e83923df494c56
princebaretto99 <princebaretto@gmail.com>
2021-03-20 13:40:30 +0530

    mostly working scraper

--- Scrapers/all_recipes.py
+++ Scrapers/all_recipes.py
@@ -14,11 +14,11 @@ recipe_ingredients = []
 recipe_intructions = []
 recipe_nutritions = []
 
-recipes_dict = dict()
+recipes_dict = []
 
 recipe_count = 0
 
-for i in range(2, 417):
+for i in range(4,10):
     
     print("Page "+str(i)+" scraping started!!!")
     
@@ -37,71 +37,75 @@ for i in range(2, 417):
     print(f"Total links in this Page are : {len(recipe_links_on_page)}")
         
     for recipe_url in recipe_links_on_page:
-        req_recipe = Request(recipe_url, headers={'User-Agent':'Mozilla/5.0'})
-        webpage_recipe = urlopen(req_recipe).read()
-        page_soup_recipe = soup(webpage_recipe,"html.parser")
-        
-        # Name of Recipe
-        name = page_soup_recipe.find('h1',class_='headline heading-content').text
-        recipe_titles.append(name)
-        
-        # Ingredients
-        ingredients = page_soup_recipe.find_all('span',class_='ingredients-item-name')
-        temp_ingredients = []
-        
-        for ingredient in ingredients:
-            temp_ingredients.append(ingredient.text.strip())
-        
-        recipe_ingredients.append(temp_ingredients)
-        
-        # Recipe 
-        recipes = page_soup_recipe.find("ul", { "class" : "instructions-section" }).findAll("li", recursive=False)
-        temp_recipe = []
-        
-        for i in range(len(recipes)):
-            temp_recipe.append(recipes[i].find('p').text)
-        
-        recipe_intructions.append(temp_recipe)
+        try :
+            req_recipe = Request(recipe_url, headers={'User-Agent':'Mozilla/5.0'})
+            webpage_recipe = urlopen(req_recipe).read()
+            page_soup_recipe = soup(webpage_recipe,"html.parser")
+            
+            # Name of Recipe
+            name = page_soup_recipe.find('h1',class_='headline heading-content').text
+            recipe_titles.append(name)
+            
+            # Ingredients
+            ingredients = page_soup_recipe.find_all('span',class_='ingredients-item-name')
+            temp_ingredients = []
+            
+            for ingredient in ingredients:
+                temp_ingredients.append(ingredient.text.strip())
+            
+            recipe_ingredients.append(temp_ingredients)
+            
+            # Recipe 
+            recipes = page_soup_recipe.find("ul", { "class" : "instructions-section" }).findAll("li", recursive=False)
+            temp_recipe = []
+            
+            for i in range(len(recipes)):
+                temp_recipe.append(recipes[i].find('p').text)
+            
+            recipe_intructions.append(temp_recipe)
 
-        # Nutrition
-        nutrition = page_soup_recipe.find('div',class_='partial recipe-nutrition-section').find('div',class_ ='section-body').text
-        recipe_nutritions.append(str(nutrition).strip())
-    
-        # Image
-        temp_img = page_soup_recipe.find("div", { "class" : "docked-sharebar-content-container" })
-        temp_img = temp_img.find("div",{"class" : "image-container"})
-        temp_img = temp_img.find("div")
+            # Nutrition
+            nutrition = page_soup_recipe.find('div',class_='partial recipe-nutrition-section').find('div',class_ ='section-body').text
+            recipe_nutritions.append(str(nutrition).strip())
         
-        recipe_img_links.append(temp_img["data-src"])
+            # Image
+            temp_img = page_soup_recipe.find("div", { "class" : "docked-sharebar-content-container" })
+            temp_img = temp_img.find("div",{"class" : "image-container"})
+            temp_img = temp_img.find("div")
+            
+            recipe_img_links.append(temp_img["data-src"])
 
-        # Data to be written 
-        dictionary ={ 
-            "title" : recipe_titles[recipe_count], 
-            "ingredients" : recipe_ingredients[recipe_count], 
-            "instructions" : recipe_intructions[recipe_count], 
-            "picture_link" : recipe_img_links[recipe_count],
-            "nutrition_text" : recipe_nutritions[recipe_count]
-        }
+            # Data to be written 
+            dictionary ={ 
+                "title" : recipe_titles[recipe_count], 
+                "ingredients" : recipe_ingredients[recipe_count], 
+                "instructions" : recipe_intructions[recipe_count], 
+                "picture_link" : recipe_img_links[recipe_count],
+                "nutrition_text" : recipe_nutritions[recipe_count]
+            }
 
-        # Done with the recipe
-        print("Recipe "+str(recipe_count)+" Scrapped")
-        recipe_count += 1
+            # Adding to recipe_dict
+            recipes_dict.append(dictionary)
 
-        # Adding to recipe_dict
-        recipes_dict[recipe_count] = dictionary
-        
+            print("saving this page ka dicks")
+
+            # Writing to dataset_ar.json 
+            with open("../Datasets/dataset_ar.json", mode = "w",  encoding='utf-8') as outfile: 
+                json.dump(recipes_dict, outfile)
+
+            # Done with the recipe
+            print("Recipe "+str(recipe_count)+" Scrapped")
+            recipe_count += 1
+        except Exception as e: 
+            print("could not scrape this recipe...")
+            print(e)
+
+    dataset_ar = pd.DataFrame(list(zip(recipe_titles, recipe_ingredients, recipe_intructions, recipe_img_links, recipe_nutritions)), 
+               columns =['title', 'ingredients', 'instructions', 'picture_link','nutrition_text'])
+
+    dataset_ar.to_csv(f'../Datasets/dataset_all_recipes_{i}.csv')
     print("Page "+str(i)+" Scraped!!!")
          
-# Storing Dataframe to JSON file format for training
-# Serializing json  
-print("######################################")
-print("All Scraping done...")
-json_object = json.dumps(recipes_dict, indent = 4) 
-  
-# Writing to dataset_ar.json 
-with open("../Datasets/dataset_ar.json", "w") as outfile: 
-    outfile.write(json_object)
-
 # Storing in CSV File
 dataset_ar = pd.DataFrame(list(zip(recipe_titles, recipe_ingredients, recipe_intructions, recipe_img_links, recipe_nutritions)), 
                columns =['title', 'ingredients', 'instructions', 'picture_link','nutrition_text'])
--- Scrapers/epicurious_recipes.py
+++ Scrapers/epicurious_recipes.py
@@ -25,7 +25,7 @@ with open("dataset_epi.json", mode='w', encoding='utf-8') as f:
 
 recipe_count = 1
 
-for i in range(2,3):# 2032):
+for i in range(3, 2032):
     
     print("Page "+str(i)+" scraping started!!!")
     
@@ -44,50 +44,61 @@ for i in range(2,3):# 2032):
     print(f"Total links in this Page are : {len(recipe_links_on_page)}")
         
     for recipe_url in recipe_links_on_page:
-        req_recipe = Request(recipe_url, headers={'User-Agent':'Mozilla/5.0'})
-        webpage_recipe = urlopen(req_recipe).read()
-        page_soup_recipe = soup(webpage_recipe,"html.parser")
+        try :
+            req_recipe = Request(recipe_url, headers={'User-Agent':'Mozilla/5.0'})
+            webpage_recipe = urlopen(req_recipe).read()
+            page_soup_recipe = soup(webpage_recipe,"html.parser")
+            
+            # Name of Recipe
+            name = page_soup_recipe.find('h1',itemprop="name").text
+            recipe_titles.append(name)
+            
+            
+            # Ingredients
+            ingredients = page_soup_recipe.find('div',class_='ingredients-info').find('ol', class_='ingredient-groups').find_all('li', {'class':'ingredient'})
+            
+            temp_ingredients = []
+            
+            for ingredient in ingredients:
+                temp_ingredients.append(ingredient.text.strip())
+            
+            recipe_ingredients.append(temp_ingredients)
+            
+            # Recipe
+            recipes = page_soup_recipe.find('div',class_='instructions').find('ol', class_='preparation-groups').find_all('li', {'class':'preparation-step'})
+            
+            temp_recipe = []
+            
+            for recipe in recipes:
+                temp_recipe.append(recipe.text.strip())
+            
+            recipe_intructions.append(temp_recipe)
         
-        # Name of Recipe
-        name = page_soup_recipe.find('h1',itemprop="name").text
-        recipe_titles.append(name)
-        
-        
-        # Ingredients
-        ingredients = page_soup_recipe.find('div',class_='ingredients-info').find('ol', class_='ingredient-groups').find_all('li', {'class':'ingredient'})
-        
-        temp_ingredients = []
-        
-        for ingredient in ingredients:
-            temp_ingredients.append(ingredient.text.strip())
-        
-        recipe_ingredients.append(temp_ingredients)
-        
-        # Recipe
-        recipes = page_soup_recipe.find('div',class_='instructions').find('ol', class_='preparation-groups').find_all('li', {'class':'preparation-step'})
-        
-        temp_recipe = []
-        
-        for recipe in recipes:
-            temp_recipe.append(recipe.text.strip())
-        
-        recipe_intructions.append(temp_recipe)
-    
-        # Image 
-        temp_img = page_soup_recipe.find('div',class_='social-img').find('img')['srcset']
-        recipe_img_links.append(temp_img)
+            # Image 
+            temp_img = page_soup_recipe.find('div',class_='social-img').find('img')['srcset']
+            recipe_img_links.append(temp_img)
 
-        with open("../Datasets/dataset_epi.json", mode='w', encoding='utf-8') as feedsjson:
-            entry = {'Recipe_number': recipe_count,'Recipe_name': name, 'Recipe_ingredients': temp_ingredients, "Recipe_instructions": temp_recipe,"Recipe_img_link" :temp_img}
-            feeds.append(entry)
-            json.dump(feeds, feedsjson)
-        
-        print("Recipe "+str(recipe_count)+" Scrapped")
-        recipe_count += 1
-        
+            with open("../Datasets/dataset_epi.json", mode='w', encoding='utf-8') as feedsjson:
+                entry = {'Recipe_number': recipe_count,'Recipe_name': name, 'Recipe_ingredients': temp_ingredients, "Recipe_instructions": temp_recipe,"Recipe_img_link" :temp_img}
+                feeds.append(entry)
+                json.dump(feeds, feedsjson)
+
+            
+            
+            print("Recipe "+str(recipe_count)+" Scrapped")
+            recipe_count += 1
+        except Exception as e:
+            print("Couldnt Parse this recipe..")
+            print(e)
+            continue
+            
+    dataset_epi = pd.DataFrame(list(zip(recipe_titles, recipe_ingredients, recipe_intructions, recipe_img_links)), 
+                columns =['Recipe_name', 'Recipe_ingredients', 'Recipe_instructions', 'Recipe_img_link'])
+
+    dataset_epi.to_csv(f'../Datasets/dataset_epi_{i}.csv')
     print("Page "+str(i)+" Scraped!!!")
         
-dataset_ar = pd.DataFrame(list(zip(recipe_titles, recipe_ingredients, recipe_intructions, recipe_img_links)), 
-               columns =['Recipe_name', 'Recipe_ingredients', 'Recipe_instructions', 'Recipe_img_link'])
+# dataset_ar = pd.DataFrame(list(zip(recipe_titles, recipe_ingredients, recipe_intructions, recipe_img_links)), 
+#                columns =['Recipe_name', 'Recipe_ingredients', 'Recipe_instructions', 'Recipe_img_link'])
 
-dataset_ar.to_csv('../Datasets/dataset_epi.csv')
+# dataset_ar.to_csv('../Datasets/dataset_epi.csv')
