3ca6b5ac31188a5b2fbc63129f05106adbbdffbf
princebaretto99 <princebaretto@gmail.com>
2021-03-20 20:10:12 +0530

    final dataset and all ingredients done

--- python.py
+++ python.py
@@ -1,32 +1,65 @@
-string = "376 calories; protein 7.3g; carbohydrates 40.4g; fat 22.4g; cholesterol 90.4mg; sodium 897.3mg. \
-                                Full Nutrition"
+import pandas as pd
+import pickle
+import numpy as np
+from textblob import TextBlob
 
-cal,sod,pro,fat,chol,carbo =[],[],[],[],[],[]
+temp_data = pd.read_csv("final_full_csv.csv")
+all_ingredients = set()
+red_words = ["tablespoon","tablespoons","cups","cup","drops","drop","teaspoon","teaspoons","½","¼","⅔","large","%","fresh","⅓","®","¾", "4\\u2009½","⅛","1\\u2009½","1\\u2009⅓","1\\u2009¼",""]
+temp_data["new_ings"] = np.nan
+for j in range(len(temp_data)):
 
-arr = string.split(";")
+    txt = str(temp_data["ingredients"][j])
 
-for i in range(len(arr)):
-    arr[i] = arr[i].strip()
-    if(i==0):
-        cal.append(arr[i].split(" ")[0])
-    elif(i==len(arr)-1):
-        sod.append(arr[i].split(" ")[1])
-    elif(i==1):
-        pro.append(arr[i].split(" ")[1])
-    elif(i==2):
-        carbo.append(arr[i].split(" ")[1])
-    elif(i==3):
-        fat.append(arr[i].split(" ")[1])
-    elif(i==4):
-        chol.append(arr[i].split(" ")[1])
+    blob = TextBlob(txt)
+    nouns = blob.noun_phrases
+
+    for i in range(len(nouns)):
+        arr = nouns[i].split(" ")
+        for word in arr:
+            if word in red_words:
+                arr.remove(word)
+        
+        
+        nouns[i] = " ".join(arr)
+
+    for i in range(len(nouns)):
+        arr = nouns[i].split(" ")
+        for word in arr:
+            if word in red_words:
+                arr.remove(word)
+        
+        if( " " in arr):
+            arr.remove(" ")
+        if("" in arr):
+            arr.remove("")
+
+        nouns[i] = " ".join(arr)
+    
+    print(f"Cleaned txt : {nouns}")
+
+    temp_data["new_ings"][j] = nouns
+    
+    for phrase in nouns:
+        all_ingredients.add(phrase)
+
+print("*************************************************************************************************")
+print(temp_data["new_ings"])
+print("*************************************************************************************************")
+
+print("*************************************************************************************************")
+print(len(temp_data))
+print("*************************************************************************************************")
+
+print("*************************************************************************************************")
+print(len(all_ingredients))
+print("*************************************************************************************************")
+
+temp_data.to_csv("ALL_RECIPES_CLEANED.CSV")
+
+with open('all_ingredients.pickle', 'wb') as f:
+    pickle.dump(all_ingredients, f)
 
 
-print(cal,sod,pro,fat,chol,carbo)
 
-df["calories"] = cal
-df["carbohydrtes"] = carbo
-df["sodium"] = sod
-df["cholestrol"] = chol
-df["fat"] = fat
-df["protiens"] = pro
 
